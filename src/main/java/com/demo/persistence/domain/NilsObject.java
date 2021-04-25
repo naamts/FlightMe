f321a5e23ef6327b0a2c901f3c233911baf622f7
naamts <38434462+naamts@users.noreply.github.com>
2021-04-26 01:45:07 +0300

    Update NilsObject.java

--- src/main/java/com/demo/persistence/domain/NilsObject.java
+++ src/main/java/com/demo/persistence/domain/NilsObject.java
@@ -1,11 +1,16 @@
 package com.demo.persistence.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NilsObject {
-
-    String Id;
+    @JsonProperty("id")
+    private String id;
 }
