f9af2ddcb0e89b0f57da098862e28122518be54f
naamts <38434462+naamts@users.noreply.github.com>
2021-04-26 01:44:14 +0300

    Update Baggage.java

--- src/main/java/com/demo/persistence/domain/Baggage.java
+++ src/main/java/com/demo/persistence/domain/Baggage.java
@@ -1,9 +1,19 @@
 package com.demo.persistence.domain;
 
-import lombok.Builder;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.NoArgsConstructor;
 
-@Builder
+@NoArgsConstructor
 public class Baggage extends NilsObject{
+    @JsonProperty("flightId")
     private String flightId;
+    @JsonProperty("suitcaseCounter")
     private Integer suitcaseCounter;
+
+    public Baggage(String id, String flightId, Integer suitCaseCount) {
+        super(id);
+        this.flightId = flightId;
+        this.suitcaseCounter = suitCaseCount;
+    }
 }
+
