9a07579908d66197841590ba694696c25ce4212d
naamts <38434462+naamts@users.noreply.github.com>
2021-04-26 01:46:21 +0300

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -2,12 +2,9 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class App {
-    private static ApplicationContext applicationContext;
-
 
     public static void main (String[] args) {
         SpringApplication.run(App.class, args);
