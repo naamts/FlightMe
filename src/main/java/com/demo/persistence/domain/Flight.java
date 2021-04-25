1aafe0d9b60523335b397d9ad59dc781ca7c2393
naamts <38434462+naamts@users.noreply.github.com>
2021-04-25 21:05:00 +0300

    Update Flight.java

--- src/main/java/com/demo/persistence/domain/Flight.java
+++ src/main/java/com/demo/persistence/domain/Flight.java
@@ -1,8 +1,15 @@
 package com.demo.persistence.domain;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class Flight extends NilsObject{
     private String destinationId;
     private Date departureDate;
+
+    Flight(String Id) {
+        super(Id);
+    }
 }
